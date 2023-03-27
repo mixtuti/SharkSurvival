287fb317e7cbb74010bdd735ede24d45059aaf3c
mixtuti桜餅 <88471532+mixtuti@users.noreply.github.com>
2023-03-27 16:36:43 +0900

    Add files via upload

---
+++ AudioManager/Scripts/BGMPath.cs
@@ -0,0 +1,12 @@
+﻿namespace AudioManager{
+
+/// <summary>
+/// BGMファイルへのパスを定数で管理するクラス
+/// </summary>
+public static class BGMPath{
+
+	public const string BGM_TMP = "BGM/bgm_tmp";
+
+}
+
+}
---
+++ AudioManager/Scripts/SEPath.cs
@@ -0,0 +1,13 @@
+﻿namespace AudioManager{
+
+/// <summary>
+/// SEファイルへのパスを定数で管理するクラス
+/// </summary>
+public static class SEPath{
+
+	public const string SE_COIN = "SE/se_Coin";
+	public const string SE_HIT  = "SE/se_Hit";
+
+}
+
+}
