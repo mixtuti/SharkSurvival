97611e05f9ef1a8835cba7da4928fc4918c58474
mixtuti桜餅 <88471532+mixtuti@users.noreply.github.com>
2023-03-27 16:39:42 +0900

    Update HPSystem.cs

--- HPSystem.cs
+++ HPSystem.cs
@@ -8,18 +8,6 @@ public class HPSystem : MonoBehaviour
     [SerializeField]
     GameObject HPGage;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void HPDown(float PlayerHP, int MaxHP)
     {
         //HPGageの中のImageというコンポーネントのfillAmountを取得して操作する
